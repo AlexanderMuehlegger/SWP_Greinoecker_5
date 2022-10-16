7314836484e4411ab362a9efc40ebfafe9ef94bd
AlexanderMuehlegger <alexmuehlegger2000@gmail.com>
2022-10-16 14:45:27 +0200

    added Player Movement -- fix required

--- Aufgabe 2/index.js
+++ Aufgabe 2/index.js
@@ -49,19 +49,23 @@ class Game {
         BONUS_REWARD: 100,
         MAX_SPEED: 2,
         BONUS_SPAWN_CHANCE: .1,
-        DEFAULT_PLAYER_SPAWN: new Vector2D(100, 100)
+        DEFAULT_PLAYER_SPAWN: new Vector2D(100, 100),
+        PLAYER_TICKS: 2,
+        PLAYER_SPEED: new Vector2D(1, 1)
     })
 
     #entities = []
 
     game_status = GAME_STATUS.STOPPED
 
-    constructor(width, height, player_spawn=DEFAULT_PLAYER_SPAWN, ctx){
+    constructor(width, height, player_spawn=this.game_config.DEFAULT_PLAYER_SPAWN, ctx){
         this.player_spawn = player_spawn
         this.width = width
         this.height = height
         this.ctx = ctx
-        this.#entities.push(new Player(this.player_spawn, new Vector2D(1,0)))
+        let player = new Player(this.player_spawn, new Vector2D(1,0), this.game_config.PLAYER_TICKS)
+        player.MAX_SPEED = this.game_config.PLAYER_SPEED
+        this.#entities.push(player)
     }
 
     #init(){
@@ -109,11 +113,13 @@ class Game {
 }
 
 class Entity {
-    constructor(position=new Vector2D(), type=ENTITY_TYPES.UNDEFINED, acc=0, speed=new Vector2D()){
+    constructor(position=new Vector2D(), type=ENTITY_TYPES.UNDEFINED, acc=new Vector2D(), speed=new Vector2D(), max_ticks){
         this.position = position
         this.type = type
         this.acc = acc
         this.speed = speed
+        this.max_ticks = max_ticks
+        this.MAX_SPEED = 0
         this.#init()
     }
 
@@ -121,7 +127,6 @@ class Entity {
     }
     
     update(){
-        console.log(this.position, this.speed)
         this.position.add(this.speed)
     }
 
@@ -141,10 +146,42 @@ class Enemy extends Entity {
 }
 
 class Player extends Entity {
-    constructor (position, speed){
-        super(position, ENTITY_TYPES.ENEMY, undefined, speed)
+    constructor (position, speed, max_ticks){
+        super(position, ENTITY_TYPES.ENEMY, undefined, speed, max_ticks)
         this.image = new Image()
         this.image.src=CONSTANTS.PLAYER_IMAGE
+        this.ticks = 0
+        window.addEventListener("keydown", (event) => this.keyHandler(event))
+    }
+
+    keyHandler(event) {
+        switch(event.code){
+            case 'KeyA': 
+                this.acc.x = -.2
+                break
+            case 'KeyS':
+                this.acc.y = -.2
+                break
+            case 'KeyD':
+                this.acc.x = .2
+                break
+            case '"KeyW':
+                this.acc.y = .2
+                break
+        }
+    }
+
+    update () {
+        if(this.ticks%this.max_ticks == 0){
+            console.log(this.acc)
+            this.speed.add(this.acc)
+            
+            this.speed.cap(this.MAX_SPEED)
+
+            this.position.add(this.speed)
+            this.ticks = 0
+        }
+        this.ticks++
     }
 }
 
@@ -167,7 +204,23 @@ class Vector2D{
         return vec2.x == this.x && vec2.y == this.y
     }
     add(vec2){
+        // console.warn(this)
         this.x += vec2.x
         this.y += vec2.y
+        // console.warn(this)
+        // console.log("-------")
+    }
+
+    cap(vec2){
+        console.warn(this)
+
+        if(Math.abs(this.x) > vec2.x)
+            this.x = this.x<0? vec2.x*-1 : vec2.x 
+        if(Math.abs(this.y) > vec2.y)
+            this.y = this.y<0? vec2.y*-1 : vec2.y 
+
+
+        console.warn(this)
+        console.log("-------")
     }
 }
