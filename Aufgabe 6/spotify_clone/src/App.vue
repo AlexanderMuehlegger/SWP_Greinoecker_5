b4700d628a9ee4ceadf42818ecd52d2fe636b5e4
AlexanderMuehlegger <alexmuehlegger2000@gmail.com>
2023-03-08 15:58:17 +0100

    working on Spotify SDK

--- Aufgabe 6/spotify_clone/src/App.vue
+++ Aufgabe 6/spotify_clone/src/App.vue
@@ -1,51 +1,18 @@
-
 <script setup>
-  import ACCESS_TOKEN from '@/assets/spotify-auth/spotify_keys'
-
-  window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = ACCESS_TOKEN;
-    const player = new Spotify.Player({
-      name: 'Web Playback SDK Quick Start Player',
-      getOAuthToken: cb => { cb(token); },
-      volume: 0.5
-    });
-  
-
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-  });
-
-  // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-  });
-
-  player.addListener('initialization_error', ({ message }) => { 
-      console.error(message);
-  });
-
-  player.addListener('authentication_error', ({ message }) => {
-      console.error(message);
-  });
-
-  player.addListener('account_error', ({ message }) => {
-      console.error(message);
-  });
-}
 </script>
 
 <template>
   <div>
-
+    <SpotifyPlayer/>
   </div>
 </template>
 
 <script>
+import SpotifyPlayer from './components/SpotifyPlayer.vue';
 export default {
   name: 'SpotifyClone',
   components: {
-
+    SpotifyPlayer
   }
 }
 </script>
